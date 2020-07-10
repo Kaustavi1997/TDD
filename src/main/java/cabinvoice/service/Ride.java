d0d3ef9d6ba2333e9b4f0eefd110071bb53624c1
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-10 18:58:29 +0530

    REFACTORED: To accomodate fares for premium rides.

--- src/main/java/cabinvoice/service/Ride.java
+++ src/main/java/cabinvoice/service/Ride.java
@@ -1,12 +1,16 @@
 package cabinvoice.service;
 
+import cabinvoice.utility.RideCategory;
+
 public class Ride {
 
     public double distance;
     public int time;
+    public RideCategory rideCategory;
 
-    public Ride(double distance, int time) {
+    public Ride(double distance, int time, RideCategory category) {
         this.distance = distance;
         this.time = time;
+        this.rideCategory = category;
     }
 }
