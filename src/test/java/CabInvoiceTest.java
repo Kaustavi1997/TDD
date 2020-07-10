4754c0a076d33fe058a6d62b86f9ad572bbf8e2a
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-10 16:39:41 +0530

    ADDED: Test case to validate output of fareCalculator method.

--- src/test/java/CabInvoiceTest.java
+++ src/test/java/CabInvoiceTest.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+import cabinvoice.service.CabInvoiceGenerator;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class CabInvoiceTest {
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
+        double distance = 3.0;
+        int time = 2;
+        double fare = cabInvoiceGenerator.fareCalculator(distance, time);
+        Assert.assertEquals(32, fare, 0.0);
+    }
 }
