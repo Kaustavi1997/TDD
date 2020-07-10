24e45ee98944abcba9790805930c447e1f60e8fe
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-10 17:08:45 +0530

    Added: Test case to validate resultant value of total fare.

--- src/test/java/CabInvoiceTest.java
+++ src/test/java/CabInvoiceTest.java
@@ -1,21 +1,37 @@
 import cabinvoice.service.CabInvoiceGenerator;
+import cabinvoice.service.Ride;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class CabInvoiceTest {
+
+    CabInvoiceGenerator cabInvoiceGenerator;
+
+    @Before
+    public void initialiser() {
+        cabInvoiceGenerator = new CabInvoiceGenerator();
+    }
+
     @Test
     public void givenDistanceAndTime_ShouldReturnTotalFare() {
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
-        double distance = 3.0;
+         double distance = 3.0;
         int time = 2;
         double fare = cabInvoiceGenerator.fareCalculator(distance, time);
         Assert.assertEquals(32, fare, 0.0);
     }
+
     @Test
     public void givenLessDistanceAndTime_ShouldReturnMinimumFare(){
-        CabInvoiceGenerator cabInvoiceGenerator = new CabInvoiceGenerator();
         double fare = cabInvoiceGenerator.fareCalculator(0.1,1);
         Assert.assertEquals(5, fare, 0.0);
     }
 
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        Ride[] rides = { new Ride(3.0, 2),
+                        new Ride(0.1, 1)};
+        double totalFare = cabInvoiceGenerator.totalFareCalculator(rides);
+        Assert.assertEquals(37, totalFare , 0.0);
+    }
 }
