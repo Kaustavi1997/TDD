ced4ae1c981b2ee2b09816d72a10c88542dd18d7
Srijan-18 <srijan.mishrasir@gmail.com>
2020-07-10 17:06:37 +0530

    Added: Declaration of Ride class and method to calculate total fare for multiple rides.

--- src/main/java/cabinvoice/service/CabInvoiceGenerator.java
+++ src/main/java/cabinvoice/service/CabInvoiceGenerator.java
@@ -4,8 +4,14 @@ public class CabInvoiceGenerator {
 
     private static final int FARE_PER_KILOMETER = 10 ;
     private static final int FARE_PER_MINUTE = 1 ;
+    private static final double MINIMUM_FARE = 5.0;
 
     public double fareCalculator(double distance, int time) {
-        return distance * FARE_PER_KILOMETER + time * FARE_PER_MINUTE;
+        double fare = distance * FARE_PER_KILOMETER + time * FARE_PER_MINUTE;
+        return Math.max(fare, MINIMUM_FARE);
+    }
+
+    public double totalFareCalculator(Ride[] rides) {
+        return 0;
     }
 }
